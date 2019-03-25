63e0a19250ccf2d05911dca9e3b8023fd5049199
lykion <546524133@qq.com>
2019-03-25 13:02:20 +0800

    重新上传

--- hello.py
+++ hello.py
@@ -3,3 +3,20 @@
 print('=============================')
 print('这是一个Django项目')
 print('=============================')
+
+list = ['a', 'b', 'c', 'd', 'e']
+print(list[4:])
+
+
+def addlist(var, list=[]):
+    list.append(var)
+    return list
+
+
+list1 = addlist(1)
+list2 = addlist(123, [])
+list3 = addlist('a')
+
+print("list1 = %s" % list1)
+print("list2 = %s " % list2)
+print("list3 = %s " % list3)
